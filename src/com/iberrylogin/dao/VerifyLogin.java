48a3ba95e82f7fef551659546a1d143c107eb5d8
Dhivya <Dhivya>
2017-02-15 14:26:00 +0530

    First commit

---
+++ src/com/iberrylogin/dao/VerifyLogin.java
@@ -0,0 +1,17 @@
+package com.iberrylogin.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class VerifyLogin {
+    public static boolean validate(String name, String pass) {
+        //This program validates the user id and password
+    	boolean status = false;
+        
+        return status;
+    }
+}
